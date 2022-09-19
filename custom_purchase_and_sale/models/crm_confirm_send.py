c013891329971193a1bb0e1f165186fa2e192ade
Erick Abrego <programador2@grupoley.com>
2022-09-19 09:18:10 -0600

    upd purchase_and_sale

--- custom_purchase_and_sale/models/crm_confirm_send.py
+++ custom_purchase_and_sale/models/crm_confirm_send.py
@@ -5,23 +5,52 @@ class CRMConfirmSend(models.TransientModel):
 
     x_is_branch_order = fields.Boolean(string="Orden sucursal?")
 
+    #Se modifica el flujo para mostrar la pantalla de confirmacion
+    def send_to_crm(self):
+        if not self.x_is_branch_order:
+            self.sale_order.write({'p_ask_for_send_to_crm':False})
+            self.sale_order.action_confirm()
+        rec = self.sale_order
+        mrp_lines = rec.order_line.filtered(lambda line: 'Fabricar' in line.product_id.route_ids.mapped('name'))
+        rule_id = rec.env["branch.factory"].sudo().search([("branch_id.id", "=", rec.company_id.id)], limit=1)
+        if rule_id:
+            purchase_id = (rec.procurement_group_id.stock_move_ids.created_purchase_line_id.order_id | rec.procurement_group_id.stock_move_ids.move_orig_ids.purchase_line_id.order_id).ids
+            purchase_id = self.env["purchase.order"].sudo().browse(purchase_id)
+            purchase_id = purchase_id[0] if purchase_id else rec.create_branch_purchase_order(rule_id, mrp_lines)
+            if purchase_id:
+                purchase_id.write(
+                    {'department_id': rule_id.department_id.id, 'partner_ref': rec.name, 'user_id': rec.user_id.id})
+                purchase_id.button_confirm()
+                sale = rec.create_factory_sale_order(rule_id, purchase_id, mrp_lines)
+                if rec.partner_shipping_id.id != rec.company_id.partner_id.id:
+                    rec.picking_ids.action_cancel()
+                    for line in rec.order_line:
+                        purchase_line = purchase_id.order_line.filtered(lambda p_line: p_line.product_id.id == line.product_id.id and line.product_uom_qty == p_line.product_qty)
+                        purchase_line["sale_line_id"] = line.id
+
+
+
+        if self.x_is_branch_order:
+            res = super(CRMConfirmSend, self).send_to_crm()
+            return res
+
     #Se modifica la función para poder seguir el flujo de crm dentro de odoo por medio de sucursal y fabrica
     def _validate_order_data(self, data):
         if self.x_is_branch_order:
-            gender = self.sale_order.partner_shipping_id.x_studio_gnero
+            gender = self.sale_order.x_branch_order_id.partner_id.x_studio_gnero
             data["datos_paciente"] = {
-                'nombre': str(self.sale_order.partner_shipping_id.name).upper(),
+                'nombre': str(self.sale_order.x_branch_order_id.partner_id.name).upper(),
                 'a_paterno': '',
                 'a_materno': '',
-                'sexo': str(dict(self.sale_order.partner_shipping_id._fields["x_studio_gnero"].selection).get(gender)).upper(),
-                'fecha_nacimiento': self.sale_order.partner_shipping_id.x_studio_cumpleaos,
-                'email': self.sale_order.partner_shipping_id.email,
-                'telefono': self.sale_order.partner_shipping_id.phone,
-                'celular': self.sale_order.partner_shipping_id.mobile,
-                'estatura': self.sale_order.partner_shipping_id.x_studio_altura_cm,
-                'peso': self.sale_order.partner_shipping_id.x_studio_peso_kgs,
-                'id_tallacalzado': float(self.sale_order.partner_shipping_id.x_studio_talla),
-                'id_sucursal': self.sale_order.branch_id.id,
+                'sexo': str(dict(self.sale_order.x_branch_order_id.partner_id._fields["x_studio_gnero"].selection).get(gender)).upper(),
+                'fecha_nacimiento': self.sale_order.x_branch_order_id.partner_id.x_studio_cumpleaos,
+                'email': self.sale_order.x_branch_order_id.partner_id.email,
+                'telefono': self.sale_order.x_branch_order_id.partner_id.phone,
+                'celular': self.sale_order.x_branch_order_id.partner_id.mobile,
+                'estatura': self.sale_order.x_branch_order_id.partner_id.x_studio_altura_cm,
+                'peso': self.sale_order.x_branch_order_id.partner_id.x_studio_peso_kgs,
+                'id_tallacalzado': float(self.sale_order.x_branch_order_id.partner_id.x_studio_talla),
+                'id_sucursal': self.sale_order.x_branch_order_id.company_id.partner_id.id,
                 "nombre_prescripcion": "",
                 "link_prescripcion":""
             }
--- custom_purchase_and_sale/models/sale_order.py
+++ custom_purchase_and_sale/models/sale_order.py
@@ -14,33 +14,11 @@ class SaleOrder(models.Model):
         for rec in self:
             if rec.x_branch_order_id:
                 if values.get("crm_status_history"):
-                    rec.x_branch_order_id.sudo().write({'crm_status_history': [(0, 0, {'status': values.get("estatus_crm"), 'date': datetime.datetime.now()})]})
+                    if rec.crm_status_history:
+                        rec.x_branch_order_id.sudo().write({'crm_status_history': [(0, 0, {'status': values.get("estatus_crm"), 'date': datetime.datetime.now()})]})
                 if values.get("estatus_crm"):
                     rec.x_branch_order_id.sudo().write({'estatus_crm': values.get("estatus_crm")})
         return res
-
-    def action_confirm(self):
-        for rec in self:
-            mrp_lines = rec.order_line.filtered(lambda line: 'Fabricar' in line.product_id.route_ids.mapped('name'))
-            rule_id = rec.env["branch.factory"].sudo().search([("branch_id.id","=",rec.company_id.id)], limit=1)
-            if rule_id:
-                rec.p_ask_for_send_to_crm = False
-
-            res = super(SaleOrder, rec).action_confirm()
-            if rule_id:
-                purchase_id = (rec.procurement_group_id.stock_move_ids.created_purchase_line_id.order_id | rec.procurement_group_id.stock_move_ids.move_orig_ids.purchase_line_id.order_id).ids
-                purchase_id = self.env["purchase.order"].sudo().browse(purchase_id)
-                purchase_id = purchase_id[0] if purchase_id else rec.create_branch_purchase_order(rule_id, mrp_lines)
-                if purchase_id:
-                    purchase_id.write({'department_id': rule_id.department_id.id, 'partner_ref': rec.name,'user_id':rec.user_id.id})
-                    purchase_id.button_confirm()
-                    sale = rec.create_factory_sale_order(rule_id, purchase_id)
-                    if self.partner_shipping_id.id != self.company_id.partner_id.id:
-                        self.picking_ids.action_cancel()
-                        for line in self.order_line:
-                            purchase_line = purchase_id.order_line.filtered(lambda p_line: p_line.product_id.id == line.product_id.id and line.product_uom_qty == p_line.product_qty)
-                            purchase_line["sale_line_id"] = line.id
-            return res
 
     def create_branch_purchase_order(self, rule_id, mrp_lines):
         if self.partner_id.x_studio_es_paciente and mrp_lines and rule_id:
@@ -61,7 +39,7 @@ class SaleOrder(models.Model):
                 purchase_id.order_line = [(0, 0, purchase_line)]
             return purchase_id
 
-    def create_factory_sale_order(self,rule_id, purchase_id):
+    def create_factory_sale_order(self,rule_id, purchase_id, mrp_lines):
         if not self.partner_id.id_crm:
             raise ValidationError(f"El paciente {self.partner_id} no cuenta con un id de CRM, favor de sincronizar e intentar de nuevo.")
         sale_data = {
@@ -79,11 +57,15 @@ class SaleOrder(models.Model):
             "x_branch_order_id": self.id
         }
         sale_order = self.env["sale.order"].sudo().create(sale_data)
-        for order_line in purchase_id.order_line:
+        for order_line in mrp_lines:
             sale_line = {
                 "product_id": order_line.product_id.id,
                 "product_uom_qty": order_line.product_uom_qty,
                 "product_uom": order_line.product_uom.id,
+                "insole_size": order_line.insole_size,
+                "top_cover_id": order_line.top_cover_id.id,
+                "design_type": order_line.design_type,
+                "analytic_tag_ids": order_line.analytic_tag_ids.ids,
             }
             sale_order.order_line = [(0,0,sale_line)]
 
